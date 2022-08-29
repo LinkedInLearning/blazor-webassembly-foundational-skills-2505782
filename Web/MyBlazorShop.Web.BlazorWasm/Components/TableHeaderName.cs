5a111f3c9bb61f877739033b7f50543c05d44a38
David Grace <davidgrace00@gmail.com>
2022-08-29 14:16:22 +0100

    Added template component

---
+++ Web/MyBlazorShop.Web.BlazorWasm/Components/TableHeaderName.cs
@@ -0,0 +1,15 @@
+﻿namespace MyBlazorShop.Web.BlazorWasm.Components
+{
+    public class TableHeaderName
+    {
+        public TableHeaderName(string name, int colspan = 1)
+        {
+            Name = name;
+            Colspan = colspan;
+        }
+
+        public int Colspan { get; }
+
+        public string Name { get;}
+    }
+}
