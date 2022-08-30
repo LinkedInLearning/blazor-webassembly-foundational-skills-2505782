dd2b64ddd6688a0fcb358220a071bf3cbc15be3e
David Grace <davidgrace00@gmail.com>
2022-08-30 19:08:42 +0100

    New methods for product service

--- Libraries/MyBlazorShop.Libraries.Services/Product/IProductService.cs
+++ Libraries/MyBlazorShop.Libraries.Services/Product/IProductService.cs
@@ -26,5 +26,20 @@ namespace MyBlazorShop.Libraries.Services.Product
         /// </summary>
         /// <returns>A <see cref="IList<ProductModel>"/> type.</returns>
         IList<ProductModel> GetAll();
+
+        /// <summary>
+        /// Gets all products, limiting to listing size
+        /// </summary>
+        /// <param name="size">The number of items to return</param>
+        /// <param name="page">The page number</param>
+        /// <returns>A <see cref="IList<ProductModel>"/> type.</returns>
+        IList<ProductModel> GetAll(int size, int page = 1);
+
+        /// <summary>
+        /// Gets the total page count for the products
+        /// </summary>
+        /// <param name="size">The number of items</param>
+        /// <returns>The total number of pages</returns>
+        int GetTotalPageCount(int size);
     }
 }
--- Libraries/MyBlazorShop.Libraries.Services/Product/ProductService.cs
+++ Libraries/MyBlazorShop.Libraries.Services/Product/ProductService.cs
@@ -51,6 +51,31 @@ namespace MyBlazorShop.Libraries.Services.Product
         {
             return _storageService.Products.ToList();
         }
+
+        /// <summary>
+        /// Gets all products, limiting to listing size
+        /// </summary>
+        /// <param name="size">The number of items</param>
+        /// <param name="page">The page number</param>
+        /// <returns>A <see cref="IList<ProductModel>"/> type.</returns>
+        public IList<ProductModel> GetAll(int size, int page = 1)
+        {
+            var skip = size * (page - 1);
+
+            return _storageService.Products.Skip(skip).Take(size).ToList();
+        }
+
+        /// <summary>
+        /// Gets the total page count for the products
+        /// </summary>
+        /// <param name="size">The number of items</param>
+        /// <returns>The total number of pages</returns>
+        public int GetTotalPageCount(int size)
+        {
+            var productCount = _storageService.Products.Count();
+
+            return productCount > 0 ? (int)Math.Ceiling((decimal)productCount / size) : 1;
+        }
     }
     
 }
