0971c90a0c2515fd9ce22d1c712fc1d6d743a87d
David Grace <davidgrace00@gmail.com>
2022-09-19 13:02:35 +0100

    Change launch settings and cache

--- Web/MyBlazorShop.Web.BlazorWasm/Program.cs
+++ Web/MyBlazorShop.Web.BlazorWasm/Program.cs
@@ -4,6 +4,8 @@ using MyBlazorShop.Web.BlazorWasm;
 using MyBlazorShop.Libraries.Services.Product;
 using MyBlazorShop.Libraries.Services.Storage;
 using MyBlazorShop.Libraries.Services.ShoppingCart;
+using System.Net.Http.Headers;
+using System.Net.Http;
 using System.Globalization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -13,6 +15,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 var http = new HttpClient()
 {
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+};
+http.DefaultRequestHeaders.CacheControl = new CacheControlHeaderValue
+{
+    NoCache = true
 };
 builder.Services.AddScoped(sp => http);
 
