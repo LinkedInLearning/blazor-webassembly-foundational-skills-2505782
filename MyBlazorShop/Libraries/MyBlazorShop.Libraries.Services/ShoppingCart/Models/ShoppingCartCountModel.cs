9ea6c846bfba71ed5c1eb5b9851f1b7d0de421db
David Grace <davidgrace00@gmail.com>
2022-09-27 22:31:28 +0100

    Added MyBlazorShop

---
+++ MyBlazorShop/Libraries/MyBlazorShop.Libraries.Services/ShoppingCart/Models/ShoppingCartCountModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlazorShop.Libraries.Services.ShoppingCart.Models
+{
+    public class ShoppingCartCountModel
+    {
+        public int Count { get; set; }
+
+        public event Action? CountChange;
+
+        public void OnCountChange()
+        {
+            CountChange?.Invoke();
+        }
+    }
+}
