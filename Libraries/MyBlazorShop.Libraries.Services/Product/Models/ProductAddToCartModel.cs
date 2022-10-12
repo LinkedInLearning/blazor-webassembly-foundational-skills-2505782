6fb4b7a25d9cf5c3c49529ba5301b3676a1f444c
David Grace <davidgrace00@gmail.com>
2022-10-12 20:45:00 +0100

    Added course video updates

---
+++ Libraries/MyBlazorShop.Libraries.Services/Product/Models/ProductAddToCartModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlazorShop.Libraries.Services.Product.Models
+{
+    public class ProductAddToCartModel
+    {
+        [Required]
+        public int? Quantity { get; set; }
+    }
+}
