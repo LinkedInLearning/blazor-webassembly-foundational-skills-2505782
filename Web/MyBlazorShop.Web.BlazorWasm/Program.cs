9c2e72e298cdbe35938bacbd51747f2dd1fb541b
David Grace <davidgrace00@gmail.com>
2022-10-15 15:02:41 +0100

    Added content from video

--- Web/MyBlazorShop.Web.BlazorWasm/Program.cs
+++ Web/MyBlazorShop.Web.BlazorWasm/Program.cs
@@ -9,7 +9,17 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var httpClient = new HttpClient
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+};
+builder.Services.AddScoped(sp => httpClient);
+
+using var response = await httpClient.GetAsync("productlisting.json");
+using var stream = await response.Content.ReadAsStreamAsync();
+
+builder.Configuration.AddJsonStream(stream);
+
 builder.Services.AddSingleton<IStorageService, StorageService>();
 builder.Services.AddSingleton<IShoppingCartService, ShoppingCartService>();
 builder.Services.AddTransient<IProductService, ProductService>();
